be7817ab3936d130935755446c1e4194c29adc0c
jun991203 <jun991203@naver.com>
2022-05-28 20:06:50 +0900

    리포트 화면 -> 일 단위만 반영

--- app/src/main/java/com/parklee/studywithcam/view/ui/main/StatisticFragment.kt
+++ app/src/main/java/com/parklee/studywithcam/view/ui/main/StatisticFragment.kt
@@ -1,20 +1,30 @@
 package com.parklee.studywithcam.view.ui.main
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import com.parklee.studywithcam.R
 import com.parklee.studywithcam.SWCapplication
 import com.parklee.studywithcam.databinding.FragmentStatisticBinding
 import com.parklee.studywithcam.model.entity.DailyStudy
+import com.parklee.studywithcam.repository.DatabaseRepository
 import com.parklee.studywithcam.view.format.ClockFormat
 import com.parklee.studywithcam.view.graph.ColorCalendar
-import com.parklee.studywithcam.view.ui.statistic.StatisticDayFragment
-import com.parklee.studywithcam.view.ui.statistic.StatisticMonthFragment
-import com.parklee.studywithcam.view.ui.statistic.StatisticWeekFragment
+import com.parklee.studywithcam.view.graph.LineMarkerView
+import com.parklee.studywithcam.view.graph.TimeAxisValueFormat
+import com.parklee.studywithcam.viewmodel.GraphViewModel
+import com.parklee.studywithcam.viewmodel.GraphViewModelFactory
+import java.text.SimpleDateFormat
+import java.util.*
 import kotlin.collections.ArrayList
 
 
@@ -25,6 +35,25 @@ class StatisticFragment : Fragment() {
 
     private val clockFormat = ClockFormat()
 
+    private val graphVM: GraphViewModel by viewModels {
+        GraphViewModelFactory((requireActivity().application as SWCapplication).dbRepository)
+    }
+
+    // 끈기 그래프
+    private var studyData = ArrayList<Entry>()
+    private var drowsinessData = ArrayList<Entry>()
+    private var spaceOutData = ArrayList<Entry>()
+
+    private var lineDataSet = ArrayList<ILineDataSet>()
+    private lateinit var lineData: LineData
+
+    // 시간 비율 그래프
+    private var timePieData = ArrayList<PieEntry>()
+    private lateinit var pieDataSet: PieDataSet
+    private lateinit var pieData: PieData
+
+    private lateinit var uid: String
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,97 +61,32 @@ class StatisticFragment : Fragment() {
         _binding = FragmentStatisticBinding.inflate(inflater, container, false)
         var view = binding.root
 
-        initFrameLayout()
         setCalendarBackground()
+
+        uid = SWCapplication.pref.getUid("uid")
+
+        graphVM.setLinearGraphData()
+
+        initTimeChart()
+        initPercentageChart()
+        initPercentageChartView()
 
         return view
     }
 
-    private fun initFrameLayout() {
-        with(binding) {
-            dayButton.isSelected = true
-            weekButton.isSelected = false
-            monthButton.isSelected = false
-        }
-        timeStatistic(1)
-        changeStatisticFragment(StatisticDayFragment())
-
-        binding.dayButton.setOnClickListener {
-            with(binding) {
-                dayButton.isSelected = true
-                weekButton.isSelected = false
-                monthButton.isSelected = false
-            }
-            timeStatistic(1)
-            changeStatisticFragment(StatisticDayFragment())
-        }
-
-        binding.weekButton.setOnClickListener {
-            with(binding) {
-                dayButton.isSelected = false
-                weekButton.isSelected = true
-                monthButton.isSelected = false
-            }
-            timeStatistic(2)
-            changeStatisticFragment(StatisticWeekFragment())
-        }
-
-        binding.monthButton.setOnClickListener {
-            with(binding) {
-                dayButton.isSelected = false
-                weekButton.isSelected = false
-                monthButton.isSelected = true
-            }
-            timeStatistic(3)
-            changeStatisticFragment(StatisticMonthFragment())
-        }
-    }
-
-    private fun changeStatisticFragment(fragment: Fragment) {
-        childFragmentManager.beginTransaction().replace(R.id.statistic_frame_layout, fragment).commit()
-    }
 
     // 시간 통계 --------------------------------------------------------------------------------
     private fun timeStatistic(unit: Int) {
-        // 1 - 일 / 2 - 주 / 3 - 월
-
         var thisStudy = 0  // 오늘, 이번 주, 이번 달 공부시간
         var lastStudy = 0  // 어제, 지난 주, 지난 달 공부시간
 
-        when(unit) {
-            1 -> {
-                thisStudy = SWCapplication.pref.getPrefTime("cTime")
-                lastStudy = 0 // DB에서 가져와야 할 값
+        thisStudy = SWCapplication.pref.getPrefTime("cTime")
+        lastStudy = 0 // DB에서 가져와야 할 값
 
-                with(binding) {
-                    timeTitle.text = getString(R.string.statistic_day_title1)
-                    timeSubTitle.text = getString(R.string.statistic_day_title2)
-                    timeComparedMe.text = getString(R.string.statistic_day_me)
-                }
-            }
-            2 -> {
-                with(binding) {
-                    thisStudy = 0
-                    lastStudy = 0 // DB에서 가져와야 할 값
-
-                    timeTitle.text = getString(R.string.statistic_week_title1)
-                    timeSubTitle.text = getString(R.string.statistic_week_title2)
-                    timeComparedMe.text = getString(R.string.statistic_week_me)
-                }
-            }
-            3 -> {
-                with(binding) {
-                    thisStudy = 0
-                    lastStudy = 0
-
-                    timeTitle.text = getString(R.string.statistic_month_title1)
-                    timeSubTitle.text = getString(R.string.statistic_month_title2)
-                    timeComparedMe.text = getString(R.string.statistic_month_me)
-                }
-            }
-            else -> {
-                Toast.makeText(context, "선택 오류", Toast.LENGTH_SHORT).show()
-            }
+        with(binding) {
+            timeTitle.text = getString(R.string.statistic_day_title1)
+            timeSubTitle.text = getString(R.string.statistic_day_title2)
+            timeComparedMe.text = getString(R.string.statistic_day_me)
         }
 
         binding.timeNow.text = clockFormat.calSecToString(thisStudy)
@@ -162,6 +126,189 @@ class StatisticFragment : Fragment() {
             binding.calendarView.addDecorators(ColorCalendar(this.requireActivity(), day))
         }
 
+    }
+
+    // ----------------------------------------------------------------------------------------
+
+    // 차트 데이터 초기화 메서드
+    private fun initTimeChartData() {
+//        graphVM.totalGraphData.observe(viewLifecycleOwner) {
+//            studyData = it
+//        }
+//        graphVM.drowsinessGraphData.observe(viewLifecycleOwner) {
+//            drowsinessData = it
+//        }
+//        graphVM.spaceOutGraphData.observe(viewLifecycleOwner) {
+//            spaceOutData = it
+//        }
+        studyData.add(Entry(-540f, 0f))
+        studyData.add(Entry((6 * 60).toFloat(), 2f))
+        studyData.add(Entry((6 * 60 + 20).toFloat(), 1f))
+        studyData.add(Entry((6 * 60 + 40).toFloat(), 2f))
+        studyData.add(Entry((6 * 60 + 100).toFloat(), 1f))
+        studyData.add(Entry((6 * 60 + 110).toFloat(), 2f))
+        studyData.add(Entry((6 * 60 + 120).toFloat(), 0f))
+        studyData.add(Entry((900).toFloat(), 0f))
+
+        spaceOutData.add(Entry(-540f, 0f))
+        spaceOutData.add(Entry((6 * 60 + 20).toFloat(), 1f))
+        spaceOutData.add(Entry((6 * 60 + 40).toFloat(), 0f))
+
+        drowsinessData.add(Entry(-540f, 0f))
+        drowsinessData.add(Entry((6 * 60 + 100).toFloat(), 1f))
+        drowsinessData.add(Entry((6 * 60 + 110).toFloat(), 0f))
+
+        val studyDataSet = LineDataSet(studyData, "onStudy")
+        studyDataSet.apply {
+            lineWidth = 2F
+            setDrawValues(false)
+            setDrawCircles(false)
+            setDrawFilled(true)
+            fillColor = ContextCompat.getColor(requireContext(), R.color.mainOrangeTrans)
+            color = ContextCompat.getColor(requireContext(), R.color.mainOrange)
+            mode = LineDataSet.Mode.STEPPED
+        }
+
+        val spaceOutDataSet = LineDataSet(spaceOutData, "spaceOut")
+        spaceOutDataSet.apply {
+            lineWidth = 2F
+            setDrawValues(false)
+            setDrawCircles(false)
+            setDrawFilled(true)
+            fillColor = ContextCompat.getColor(requireContext(), R.color.purple200Trans)
+            color = ContextCompat.getColor(requireContext(), R.color.purple_200)
+            mode = LineDataSet.Mode.STEPPED
+        }
+
+        val drowsinessDataSet = LineDataSet(drowsinessData, "drowsiness")
+        drowsinessDataSet.apply {
+            lineWidth = 2F
+            setDrawValues(false)
+            setDrawCircles(false)
+            setDrawFilled(true)
+            fillColor = ContextCompat.getColor(requireContext(), R.color.teal200Trans)
+            color = ContextCompat.getColor(requireContext(), R.color.teal_200)
+            mode = LineDataSet.Mode.STEPPED
+        }
+
+        lineDataSet.add(studyDataSet)
+        lineDataSet.add(spaceOutDataSet)
+        lineDataSet.add(drowsinessDataSet)
+        lineData = LineData(lineDataSet)
+    }
+
+    // 차트 초기화 메서드
+    private fun initTimeChart() {
+        initTimeChartData()
+
+        binding.layoutGraph.studyChart.run {
+            setDrawGridBackground(false)
+            setBackgroundColor(Color.WHITE)
+            legend.isEnabled = false
+            isDragYEnabled = false
+        }
+
+        with(binding.layoutGraph.studyChart.xAxis) {
+            setDrawLabels(true)  // Label 표시 여부
+            axisMaximum = 900f  // 60min * 24hour
+            axisMinimum = -540f
+            labelCount = 5
+            valueFormatter = TimeAxisValueFormat()
+
+            textColor = android.graphics.Color.BLACK
+            position = com.github.mikephil.charting.components.XAxis.XAxisPosition.BOTTOM  // x축 라벨 위치
+            setDrawLabels(true)  // Grid-line 표시
+            setDrawAxisLine(true)  // Axis-Line 표시
+        }
+
+        // 왼쪽 y축 값
+        with(binding.layoutGraph.studyChart.axisLeft) {
+            axisMaximum = 2.2f   // y축 최대값(고정)
+            axisMinimum = 0.1f  // y축 최소값(고정)
+
+            // 왼쪽 y축 도메인 변경
+            val yAxisVals = ArrayList<String>(listOf("-", "X", "O"))
+            valueFormatter = IndexAxisValueFormatter(yAxisVals)
+            granularity = 1f
+        }
+
+        // 오른쪽 y축 값
+        with(binding.layoutGraph.studyChart.axisRight) {
+            setDrawLabels(false)
+            setDrawAxisLine(false)
+            setDrawGridLines(false)
+        }
+
+        val marker = LineMarkerView(requireContext(), R.layout.graph_marker)
+        marker.chartView = binding.layoutGraph.studyChart
+        binding.layoutGraph.studyChart.marker = marker
+
+        binding.layoutGraph.studyChart.description.isEnabled = false  // 설명
+        binding.layoutGraph.studyChart.data = lineData
+
+        binding.layoutGraph.studyChart.invalidate()  // 다시 그리기
+    }
+
+    private fun initPercentageChartData() {
+        timePieData.add(PieEntry(80f, requireActivity().getString(R.string.state_study)))
+        timePieData.add(PieEntry(15f, requireActivity().getString(R.string.state_drowsiness)))
+        timePieData.add(PieEntry(5f, requireActivity().getString(R.string.state_space_out)))
+
+        pieDataSet = PieDataSet(timePieData, "Time Percentage")
+        val pieColor = arrayListOf<Int>(
+            ContextCompat.getColor(requireActivity(), R.color.mainOrange),
+            ContextCompat.getColor(requireActivity(), R.color.teal_200),
+            ContextCompat.getColor(requireActivity(), R.color.purple_200))
+        with(pieDataSet) {
+            sliceSpace = 3f
+            colors = pieColor
+        }
+        pieData = PieData(pieDataSet)
+        with(pieData) {
+            setValueTextSize(11f)
+            setValueTextColor(android.graphics.Color.WHITE)
+        }
+    }
+
+    private fun initPercentageChart() {
+        initPercentageChartData()
+
+        binding.layoutGraph.studyTimeChart.run {
+            transparentCircleRadius = 95f
+            description.isEnabled = false
+            setUsePercentValues(true)
+            setDrawEntryLabels(false)
+
+            isDrawHoleEnabled = true
+            setHoleColor(Color.WHITE)
+
+            legend.isEnabled = false
+            data = pieData
+        }
+
+
+    }
+
+    private fun initPercentageChartView() {
+        with(binding.layoutGraph.layoutTimeStudy) {
+            tvTimeTitle.text = getString(com.parklee.studywithcam.R.string.state_study)
+            tvTime.text = "00 : 00 : 00"
+        }
+        with(binding.layoutGraph.layoutTimeDrowsiness) {
+            tvTimeTitle.text = getString(com.parklee.studywithcam.R.string.state_drowsiness)
+            tvTime.text = "00 : 00 : 00"
+        }
+        with(binding.layoutGraph.layoutTimeSpaceOut) {
+            tvTimeTitle.text = getString(com.parklee.studywithcam.R.string.state_space_out)
+            tvTime.text = "00 : 00 : 00"
+        }
+    }
+
+
+    private fun getTodayDate(): String {
+        var date = Date(System.currentTimeMillis())
+        var todayDateFormat = SimpleDateFormat("yyyy-MM-dd")
+        return todayDateFormat.format(date)
     }
 
 }
